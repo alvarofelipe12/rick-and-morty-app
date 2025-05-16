e0c163b8d15f7ae8bc3cd079fc1c214ebb58af3f
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2025-05-16 15:00:06 -0500

    Adding dependencies
    - Added Tailwind.
    - Added Axios.
    - Added react-router.
    - Moved testing libraries as dev dependencies.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#11B0C8",
+        secondary: "#3C3E44",
+        background: "#F5F5F5",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
